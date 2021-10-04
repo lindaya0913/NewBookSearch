403174cd0a07c7a38a3cdd4a2450ec0e3dfda713
lindaya0913 <lindy0913@gmail.com>
2021-10-04 17:43:11 +0800

    add search, url func & connect json

--- controller.js
+++ controller.js
@@ -1,18 +1,49 @@
 window.onload = function () {
-    console.log("11111")
-    var formData = $('#123').serializeArray();
+    var formData = $('#searchform').serializeArray();
     console.log("formData", formData)
-    $('#123').submit(function() {
-        // get all the inputs into an array.
-        var $inputs = $('#123 :input');
-    
-        // not sure if you wanted this, but I thought I'd add it.
-        // get an associative array of just the values.
-        var values = {};
-        $inputs.each(function() {
-            values[this.name] = $(this).val();
-        });
-        console.log("values", values)
-    });
-
+    let url = "09.json";
+    let request = new XMLHttpRequest();
+    request.open("get", url);
+    request.send(null);
+    request.onload = function () {
+        if (request.status == 200) {
+            var json = JSON.parse(request.responseText);
+            var el = document.querySelector(".list");
+            var str = ""; 
+            x=0;
+            for (var i = 0; i < json.length; i++) {
+                if(formData[1].value != 'All' & formData[2].value != 'All'){
+                    if(json[i].Column1 == formData[1].value & json[i].Column4 == formData[2].value){
+                        var content = "<li><a href='https://nthu.primo.exlibrisgroup.com/discovery/fulldisplay?docid=alma" + json[i].Column2 + "&context=L&vid=886UST_NTHU:886UST_NTHU'>" + json[i].Column3 + "</a></li>";
+                        str += content;
+                        x++;
+                    }else{
+                        continue
+                    }
+                }else if(formData[1].value != 'All'){
+                    if(json[i].Column1 == formData[1].value){
+                        var content = "<li><a href='https://nthu.primo.exlibrisgroup.com/discovery/fulldisplay?docid=alma" + json[i].Column2 + "&context=L&vid=886UST_NTHU:886UST_NTHU'>" + json[i].Column3 + "</a></li>";
+                        str += content;
+                        x++;
+                    }else{
+                        continue
+                    }
+                }else if(formData[2].value != 'All'){
+                    if(json[i].Column4 == formData[2].value){
+                        var content = "<li><a href='https://nthu.primo.exlibrisgroup.com/discovery/fulldisplay?docid=alma" + json[i].Column2 + "&context=L&vid=886UST_NTHU:886UST_NTHU'>" + json[i].Column3 + "</a></li>";
+                        str += content;
+                        x++;
+                    }else{
+                        continue
+                    }
+                }else{
+                    var content = "<li><a href='https://nthu.primo.exlibrisgroup.com/discovery/fulldisplay?docid=alma" + json[i].Column2 + "&context=L&vid=886UST_NTHU:886UST_NTHU'>" + json[i].Column3 + "</a></li>";
+                    str += content;
+                    x++;
+                }
+            }
+            console.log(x)
+            el.innerHTML = str;
+        }
+    }
 }
