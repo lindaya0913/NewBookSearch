cb8e600d25aaf16895e6b78b0fa36b08b2b9a24b
lindaya0913 <lindy0913@gmail.com>
2021-10-09 15:41:04 +0800

    add new tab & tips without information

--- controller.js
+++ controller.js
@@ -9,12 +9,12 @@ window.onload = function () {
         if (request.status == 200) {
             var json = JSON.parse(request.responseText);
             var el = document.querySelector(".list");
-            var str = ""; 
-            x=0;
+            var str = ''; 
+            x = 0;
             for (var i = 0; i < json.length; i++) {
                 if(formData[1].value != 'All' & formData[2].value != 'All'){
                     if(json[i].Column1 == formData[1].value & json[i].Column4 == formData[2].value){
-                        var content = "<li><a href='https://nthu.primo.exlibrisgroup.com/discovery/fulldisplay?docid=alma" + json[i].Column2 + "&context=L&vid=886UST_NTHU:886UST_NTHU'>" + json[i].Column3 + "</a></li>";
+                        var content = "<li><a href='https://nthu.primo.exlibrisgroup.com/discovery/fulldisplay?docid=alma" + json[i].Column2 + "&context=L&vid=886UST_NTHU:886UST_NTHU' target='_blank'>" + json[i].Column3 + "</a></li>";
                         str += content;
                         x++;
                     }else{
@@ -22,7 +22,7 @@ window.onload = function () {
                     }
                 }else if(formData[1].value != 'All'){
                     if(json[i].Column1 == formData[1].value){
-                        var content = "<li><a href='https://nthu.primo.exlibrisgroup.com/discovery/fulldisplay?docid=alma" + json[i].Column2 + "&context=L&vid=886UST_NTHU:886UST_NTHU'>" + json[i].Column3 + "</a></li>";
+                        var content = "<li><a href='https://nthu.primo.exlibrisgroup.com/discovery/fulldisplay?docid=alma" + json[i].Column2 + "&context=L&vid=886UST_NTHU:886UST_NTHU' target='_blank'>" + json[i].Column3 + "</a></li>";
                         str += content;
                         x++;
                     }else{
@@ -30,19 +30,24 @@ window.onload = function () {
                     }
                 }else if(formData[2].value != 'All'){
                     if(json[i].Column4 == formData[2].value){
-                        var content = "<li><a href='https://nthu.primo.exlibrisgroup.com/discovery/fulldisplay?docid=alma" + json[i].Column2 + "&context=L&vid=886UST_NTHU:886UST_NTHU'>" + json[i].Column3 + "</a></li>";
+                        var content = "<li><a href='https://nthu.primo.exlibrisgroup.com/discovery/fulldisplay?docid=alma" + json[i].Column2 + "&context=L&vid=886UST_NTHU:886UST_NTHU' target='_blank'>" + json[i].Column3 + "</a></li>";
                         str += content;
                         x++;
                     }else{
                         continue
                     }
                 }else{
-                    var content = "<li><a href='https://nthu.primo.exlibrisgroup.com/discovery/fulldisplay?docid=alma" + json[i].Column2 + "&context=L&vid=886UST_NTHU:886UST_NTHU'>" + json[i].Column3 + "</a></li>";
+                    var content = "<li><a href='https://nthu.primo.exlibrisgroup.com/discovery/fulldisplay?docid=alma" + json[i].Column2 + "&context=L&vid=886UST_NTHU:886UST_NTHU' target='_blank'>" + json[i].Column3 + "</a></li>";
                     str += content;
                     x++;
                 }
             }
-            console.log(x)
+
+            if(str == ''){
+                str += "<h4>無新到館藏!!</h4>"
+            }
+
+            console.log('x', x)
             el.innerHTML = str;
         }
     }
