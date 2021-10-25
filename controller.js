fe3679ef796e638de29d75311590fa998642b96d
lindaya0913 <lindy0913@gmail.com>
2021-10-25 15:51:31 +0800

    month can change automatically

--- controller.js
+++ controller.js
@@ -2,13 +2,60 @@ window.onload = function () {
     var formData = $('#searchform').serializeArray();
     console.log("formData", formData)
 
-    let month = {'2021/4':'resource/04.json','2021/5':'resource/05.json','2021/6':'resource/06.json',
-                 '2021/7':'resource/07.json','2021/8':'resource/08.json','All':'resource/09.json'};
+    var Today = new Date();
+    var Tyear = Today.getFullYear(); 
+    var Tmonth = Today.getMonth(); 
+    var Oyear = Today.getFullYear() - 1; 
+    var outstr = {}; 
+    j = Tmonth; 
+    if(Tmonth < 10){
+        j = "0" + j; 
+    }else{ 
+        j = "" + j; 
+    } 
+    outstr['All'] = "resource/" + j + ".json"; 
+
+    // Determine whether this month is within May, if it is an option, it needs to be pushed back to last year.
+    if (Tmonth <= 5){
+        // This year
+        for(i = Tmonth - 1; i > 0; i--) { 
+            j = i; 
+            if(j < 10) { 
+                j = "0" + j; 
+            } else { 
+                j = "" + j; 
+            } 
+            jj = Tyear + "/" + j; 
+            outstr[jj] = "resource/" + j + ".json";
+        } 
+        // Last year
+        for(i = 12; i > (12 + (Tmonth - 6)); i--){ 
+            j = i; 
+            if(j < 10) { 
+                j = "0" + j; 
+            } else { 
+                j = "" + j; 
+            } 
+            jj = Oyear + "/" + j; 
+            outstr[jj] = "resource/" + j + ".json"; 
+        } 
+    }else{
+        for(i = Tmonth - 1; i >= (Tmonth - 5); i--) { 
+            j = i; 
+            if(j < 10) { 
+                j = "0" + j; 
+            } else { 
+                j = "" + j; 
+            } 
+            jj = Tyear + "/" + j; 
+            outstr[jj] = "resource/" + j + ".json";
+        } 
+    }
 
     let url = '';
-    for(key in month){
+    for(key in outstr){
         if(formData[0].value == key){
-            url = month[key];
+            url = outstr[key];
         }
     }
     console.log("url", url)
